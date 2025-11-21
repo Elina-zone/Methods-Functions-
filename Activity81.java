597f66840d53d0c47b1e9ed7a8e70b115aab279d
Elina-zone <Elinamughal8@gmail.com>
2025-11-21 19:16:58 +0500

    This is my first change

--- Activity81.java
+++ Activity81.java
@@ -1,10 +1,10 @@
 public class Activity81{ 
     /** Main method */ 
     public static void main(String[] args) { 
-	int i = 5; 
-	int j = 2; 
-	int k = max(i, j); //Method calling 
-	System.out.println("The maximum of " + i + " and " + j + " is " + k); 
+	int a = 5; 
+	int b = 2; 
+	int c = max(a, b); //Method calling 
+	System.out.println("The maximum of " + a + " and " + b + " is " + c); 
     } 
     /** Return the max of two numbers – Method Definition*/ 
     public static int max(int num1, int num2) { 
